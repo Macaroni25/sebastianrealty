4f71ff31ccbfee5ea95b08f7d579a854f0a4c4f2
Alex Weininger <alex.weininger@live.com>
2016-01-01 11:14:54 -0500

    navbar in sebastian.php

--- sebastian.php
+++ sebastian.php
@@ -12,21 +12,9 @@
 </head>
 
 <body>
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo">Sebastain Realty</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="index.html">Properties and Rentals</a></li>
-        <li><a href="sebastian.html">Sebastian Information</a></li>
-        <li><a href="about.html">About us and our staff</a></li>
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
+  <?
+  include "navbar.php"
+  ?>
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br>
