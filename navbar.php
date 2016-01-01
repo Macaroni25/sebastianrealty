c144f1c6b218d2364cfa79c46ecfb65c3014050c
Alex Weininger <alex.weininger@live.com>
2016-01-01 10:59:52 -0500

    v 0.13
    renamed index.html -> index.php and made navbar.php

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo">Sebastain Realty</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.html">Home</a></li>
+                <li><a href="listings.html">Properties and Rentals</a></li>
+                        <li><a href="sebastian.html">Sebastian Information</a></li>
+        <li><a href="about.html">About us and our staff</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br>
+      <br>
+      <h1 class="header center orange-text">Sebastian Realty</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Fifty years of Real Estate expierence, at your service.</h5>
+      </div>
+      <div class="row center">
+        <a href="about.html" id="download-button" class="btn-large waves-effect waves-light orange">Learn More</a>
+      </div>
+      <br>
+      <br>
+
+    </div>
+  </div>
