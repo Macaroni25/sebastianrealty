a06b7408be8f2055c70bca148908c7e516cb69f5
Alex Weininger <alex.weininger@live.com>
2016-01-02 16:47:47 -0500

    contact info
    bio.php

---
+++ bio.php
@@ -0,0 +1,79 @@
+<!-- Gil -->
+<div class="paddingbox">
+  <div class="row">
+    <div class="card-panel">
+        <p class="biotitle">Gilbert J. Swiger, Broker and Owner of Sebastian Realty, Inc. Realtor®, GRI, CRS, IBBA and BBF</p>
+        <div class="row">
+          <div class="col s8">
+      <div id="biocard" class="card">
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Gil Swiger</span>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gil's been in the Sebastian area since 1968, coming here from Ohio. He negotiated, formed and installed Sebastian's first cable TV company, Indian River Cablevision, in 1970. In 1976, after selling the cablevision company, Gil became actively involved as a broker in real estate. He resides in Sebastian with his broker/wife Rhoda.</p>
+          <br>
+          <p><a class="link1" href="http://swigergi.rairc.mlxchange.com/">Listings</a></p>
+        </div>
+
+      </div>
+    </div>
+    <div class="col s4">
+      <div id="biocard" class="card">
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Contact Info</span>
+          <br>
+          <br>
+            <p>Email:  gswiger2@gmail.com</p>
+            <br>
+            <p>Phone:  772-589-3111</p>
+            <br>
+            <p>Cell:  772-473-7411</p>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
+<!-- Rhoda -->
+<div class="paddingbox">
+  <div class="row">
+    <div class="card-panel">
+        <p class="biotitle">Rhoda Swiger, Broker of Atha & Swiger Real Estate Inc.</p>
+        <div class="row">
+          <div class="col s8">
+      <div id="biocard" class="card">
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Rhoda Swiger</span>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rhoda also comes from Ohio, she handles the annual and seasonal rentals through A.S.R.E., and is also the manager of Sebastian Realty Inc. Her background as a school teacher has given her excellent organizational skills, as well as a n easy-going manner with people. She has been active in real estate since 1979. Gil and Rhoda have been married since 1966. They have a son, Jeff, also in Sebastian Florida and a daughter Sue. Sue , her husband Chuck and 2 grandsons live in Redmond Washington.</p>
+          <br>
+          <p><a class="link1" href="http://swigergi.rairc.mlxchange.com/">Listings</a></p>
+        </div>
+
+      </div>
+    </div>
+    <div class="col s4">
+      <div id="biocard" class="card">
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Contact Info</span>
+          <br>
+          <br>
+            <p>Email:  Rswiger2@gmail.com</p>
+            <br>
+            <p>Cell:  772-473-7459</p>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
+<!-- Jeff -->
+
+<!--  -->
+
+                        <!-- Gil -->
+
+                              <!-- Gil -->
