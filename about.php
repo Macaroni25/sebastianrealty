4ee45b12ccb1e52226415f20b5d186a28a6ec901
Alex Weininger <alex.weininger@live.com>
2016-01-01 20:28:33 -0500

    navbar fix

--- about.php
+++ about.php
@@ -28,17 +28,9 @@ $("*").on("click", function() {
     </script>
 </head>
 <body>
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo">Sebastain Realty</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">About</a></li>
-      </ul>
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
+<?
+ include "navbar.php"
+?>
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br>
